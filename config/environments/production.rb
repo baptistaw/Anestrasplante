1d3e2ab691337f99c24ed325eee9488abdaebf38
William <baptistaw@gmail.com>
2016-12-23 23:21:53 -0300

    Reparacion de config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,15 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.default_url_options = { host: 'https://intense-tor-59410.herokuapp.com' }
   # Code is not reloaded between requests.
-config.action_mailer.default_url_options =   { :host => 'https://intense-tor-59410.herokuapp.com' }
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-      address: "smtp.gmail.com",
-      port: 587,
-      authentication: "plain",
-      enable_starttls_auto: true,
-      user_name: "baptistaw@gmail.com",
-      password: "w4m4b5m2" 
+
 
   config.cache_classes = true
 
