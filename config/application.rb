d1bd94beed5df6a5348cc4a89f2b3bea3fa8bdc0
William <baptistaw@gmail.com>
2016-12-25 14:50:12 -0300

    cambio de collapsible a tabs 3a aprox

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module Trasplantedecero
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.default_timezone
     config.active_record.raise_in_transactional_callbacks = true
-    config.serve_static_assets = true
+    config.serve_static_files = true
 
   end
 end
